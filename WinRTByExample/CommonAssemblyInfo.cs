777834f1119fc8e000c566631e126a838f4c8cd8
Jeremy Likness <jeremy@jeremylikness.com>
2013-01-09 12:02:26 +0000

    Initial set up

---
+++ WinRTByExample/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Jeremy Likness")]
+[assembly: AssemblyCopyright("Copyright (c) Jeremy Likness 2013")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: CLSCompliant(true)]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
