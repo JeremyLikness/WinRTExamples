a63c6c0044db980590bacec8415d6a5be83c102b
jeremylikness_cp <SND\jeremylikness_cp>
2013-02-01 01:55:26 +0000

    Chapter 2 WinRT examples

---
+++ WinRTByExample/MyLibrary/WordSplitter.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WordSplitter.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The word splitter.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace MyLibrary
+{
+    using System.Linq;
+
+    /// <summary>
+    /// The word splitter.
+    /// </summary>
+    public sealed class WordSplitter
+    {
+        /// <summary>
+        /// The split.
+        /// </summary>
+        /// <param name="source">
+        /// The source.
+        /// </param>
+        /// <returns>
+        /// The <see cref="string"></see> array
+        /// </returns>
+        public string[] Split(string source)
+        {
+            return (from word in source.Split(' ') 
+                    where !string.IsNullOrEmpty(word) 
+                    orderby word 
+                    select word).ToArray();
+        }
+    }
+}
