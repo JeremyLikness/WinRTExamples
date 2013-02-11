8522b281984fbcec6665502d183b635fdbc88538
jeremylikness_cp <SND\jeremylikness_cp>
2013-02-11 12:49:36 +0000

    Chapter 3 animations example

---
+++ WinRTByExample/AnimationsExample/ColorAnimationType.cs
@@ -0,0 +1,63 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ColorAnimationType.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The color animation type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AnimationsExample
+{
+    using System;
+
+    using Windows.UI;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Media.Animation;
+
+    /// <summary>
+    /// The bounce ease type.
+    /// </summary>
+    public class ColorAnimationType : BaseAnimationType
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ColorAnimationType"/> class. 
+        /// </summary>
+        public ColorAnimationType()
+        {
+            this.Description = "Color Animation";
+        }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public override sealed string Description { get; protected set; }
+
+        /// <summary>
+        /// The start animation.
+        /// </summary>
+        /// <param name="target">
+        /// The target.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Storyboard"/>.
+        /// </returns>
+        public override Storyboard GenerateAnimation(FrameworkElement target)
+        {            
+            var colorAnimation = new ColorAnimation
+            {
+                From = Colors.Red,
+                To = Colors.Blue,
+                Duration = TimeSpan.FromSeconds(5)                
+            };
+
+            Storyboard.SetTarget(colorAnimation, target);
+            Storyboard.SetTargetProperty(
+                colorAnimation,
+                "(FrameworkElement.Fill).(SolidColorBrush.Color)");
+            var storyboard = new Storyboard();           
+            storyboard.Children.Add(colorAnimation);
+            return storyboard;
+        }
+    }
+}
---
+++ WinRTByExample/AnimationsExample/IAnimationType.cs
@@ -0,0 +1,41 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IAnimationType.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The AnimationType interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AnimationsExample
+{
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Media.Animation;
+
+    /// <summary>
+    /// The AnimationType interface.
+    /// </summary>
+    public interface IAnimationType
+    {
+        /// <summary>
+        /// Gets the id.
+        /// </summary>
+        int Id { get; }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        string Description { get; }
+
+        /// <summary>
+        /// The start animation.
+        /// </summary>
+        /// <param name="target">
+        /// The target.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Storyboard"/>.
+        /// </returns>
+        Storyboard GenerateAnimation(FrameworkElement target);
+    }
+}
