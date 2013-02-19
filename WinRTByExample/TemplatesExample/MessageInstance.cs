27af60d989711b45e9cc92d483fe685565db498a
jeremylikness_cp <SND\jeremylikness_cp>
2013-02-19 15:34:02 +0000

    Chapter 3 template example

---
+++ WinRTByExample/TemplatesExample/MessageInstance.cs
@@ -0,0 +1,18 @@
+﻿namespace TemplatesExample
+{
+    /// <summary>
+    /// An instance of a message which includes the text and a type
+    /// </summary>
+    public class MessageInstance
+    {
+        /// <summary>
+        /// Gets or sets the type of the message
+        /// </summary>
+        public MessageType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text of the message
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
