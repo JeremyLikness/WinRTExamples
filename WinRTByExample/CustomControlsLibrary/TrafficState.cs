e1d853b837333e8de99825c42e268e5ea632b65b
jeremylikness_cp <SND\jeremylikness_cp>
2013-02-24 17:28:34 +0000

    Chapter 3 Custom Control example

---
+++ WinRTByExample/CustomControlsLibrary/TrafficState.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TrafficState.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The traffic state.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace CustomControlsLibrary
+{
+    /// <summary>
+    /// The traffic state.
+    /// </summary>
+    public enum TrafficState
+    {
+        /// <summary>
+        /// The off.
+        /// </summary>
+        Off,
+
+        /// <summary>
+        /// The red.
+        /// </summary>
+        Red,
+
+        /// <summary>
+        /// The yellow.
+        /// </summary>
+        Yellow,
+
+        /// <summary>
+        /// The green.
+        /// </summary>
+        Green
+    }
+}
