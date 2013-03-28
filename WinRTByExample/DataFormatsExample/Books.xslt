9bf96923542c9db616c0fa993732e78573212c2d
Jeremy Likness <jeremy@jeremylikness.com>
2013-03-28 21:46:44 +0000

    Chapter 4 Data and Document formatting examples

---
+++ WinRTByExample/DataFormatsExample/Books.xslt
@@ -0,0 +1,12 @@
+﻿<?xml version="1.0"?>
+<xsl:stylesheet version="1.0"
+    xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:template match="catalog">
+    <xsl:for-each select="book">
+      <xsl:value-of select="title"/> - <xsl:value-of select="price"/>
+      by <xsl:value-of select="author"/>
+            <xsl:value-of select="description"/>
+              (published on <xsl:value-of select="publish_date"/>)            
+        </xsl:for-each>      
+  </xsl:template>
+</xsl:stylesheet>
---
+++ WinRTByExample/DataFormatsExample/MainPage.xaml.cs
@@ -0,0 +1,118 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainPage.xaml.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   An empty page that can be used on its own or navigated to within a Frame.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DataFormatsExample
+{
+    using System;
+    
+    using Windows.Data.Json;
+    using Windows.Data.Xml.Dom;
+    using Windows.Data.Xml.Xsl;
+    using Windows.Storage;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Documents;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        /// <summary>
+        /// The books.
+        /// </summary>
+        private readonly Uri booksUri = new Uri("ms-appx:///Books.xml");
+
+        /// <summary>
+        /// The books XSLT 
+        /// </summary>
+        private readonly Uri booksXslt = new Uri("ms-appx:///Books.xslt");
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// The parse book.
+        /// </summary>
+        /// <param name="book">
+        /// The book.
+        /// </param>
+        /// <returns>
+        /// The <see cref="JsonObject"/>.
+        /// </returns>
+        private static JsonObject ParseBook(XmlElement book)
+        {
+            var obj = new JsonObject { { "id", JsonValue.CreateStringValue(book.GetAttribute("id")) } };
+            foreach (var node in book.ChildNodes)
+            {
+                var element = node as XmlElement;
+                if (element != null)
+                {
+                    obj.Add(element.NodeName, JsonValue.CreateStringValue(element.InnerText));
+                }
+            }
+
+            return obj;
+        }
+
+        /// <summary>
+        /// The parse_ on click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private async void Parse_OnClick(object sender, RoutedEventArgs e)
+        {
+            var file = await StorageFile.GetFileFromApplicationUriAsync(this.booksUri);
+            var xml = await XmlDocument.LoadFromFileAsync(file);
+
+            var json = new JsonObject();
+            var catalog = xml.DocumentElement;
+            if (catalog != null)
+            {
+                var books = new JsonArray();
+                foreach (var childNode in catalog.GetElementsByTagName("book"))
+                {
+                    books.Add(ParseBook((XmlElement)childNode));
+                }
+
+                json.Add("catalog", books);
+            }
+
+            var jsonText = json.Stringify();
+            TransformedText.Text = jsonText;
+        }
+
+        /// <summary>
+        /// The transform_ on click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private async void Transform_OnClick(object sender, RoutedEventArgs e)
+        {
+            var xsltFile = await StorageFile.GetFileFromApplicationUriAsync(this.booksXslt);
+            var xslt = await XmlDocument.LoadFromFileAsync(xsltFile);
+            var processor = new XsltProcessor(xslt);
+            var xmlFile = await StorageFile.GetFileFromApplicationUriAsync(this.booksUri);
+            var xml = await XmlDocument.LoadFromFileAsync(xmlFile);
+            TransformedText.Text = processor.TransformToString(xml.DocumentElement);
+        }        
+    }
+}
