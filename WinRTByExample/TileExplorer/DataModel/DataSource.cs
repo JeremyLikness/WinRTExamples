8ad3f7b715ed9647a4d3ce2a86f69d26931e1870
Jeremy Likness <jeremy@jeremylikness.com>
2013-05-04 16:32:22 +0000

    Initial content for tile explorer

---
+++ WinRTByExample/TileExplorer/DataModel/DataSource.cs
@@ -0,0 +1,49 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="DataSource.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The data source.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TileExplorer.DataModel
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using TileExplorer.Tiles;
+
+    /// <summary>
+    /// The data source.
+    /// </summary>
+    public class DataSource
+    {
+        /// <summary>
+        /// The groups.
+        /// </summary>
+        private readonly List<TileGroup> groups = new List<TileGroup>();
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataSource"/> class.
+        /// </summary>
+        public DataSource()
+        {
+            var tileList = TileHelper.GetTiles();
+            var squares = new TileGroup { Name = "Square" };
+            foreach (var tile in tileList.Where(tile => tile.TileType.Equals(TileTypes.Square)))
+            {
+                squares.Items.Add(new TileItem(tile));
+            }
+
+            var wides = new TileGroup { Name = "Wide" };
+            foreach (var tile in tileList.Where(tile => tile.TileType.Equals(TileTypes.Wide)))
+            {
+                wides.Items.Add(new TileItem(tile));
+            }
+
+            this.groups.Add(squares);
+            this.groups.Add(wides);
+        }
+    }
+}
---
+++ WinRTByExample/TileExplorer/Tiles/TileTypes.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="TileTypes.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   Type of the tile
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TileExplorer.Tiles
+{
+    /// <summary>
+    /// Type of the tile 
+    /// </summary>
+    public enum TileTypes
+    {
+        /// <summary>
+        /// Square tile
+        /// </summary>
+        Square, 
+
+        /// <summary>
+        /// Wide tile
+        /// </summary>
+        Wide
+    }
+}
