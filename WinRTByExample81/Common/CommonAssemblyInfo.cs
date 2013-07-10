dc78a759a57ed08b2e77d40b1b3723245bdf3936
Jeremy Likness <jeremy@jeremylikness.com>
2013-07-10 23:06:54 +0000

    New solution for Windows 8.1

---
+++ WinRTByExample81/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("Jeremy Likness")]
+[assembly: AssemblyCopyright("Copyright (c) Jeremy Likness 2013")]
+[assembly: AssemblyTrademark("")]
+
+#if !NETFX_CORE
+[assembly: CLSCompliant(true)]
+#endif
+
+#if !PORTABLE
+[assembly: ComVisible(false)]
+#endif
+
+[assembly: AssemblyVersion("1.0.0.0")]
+
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
