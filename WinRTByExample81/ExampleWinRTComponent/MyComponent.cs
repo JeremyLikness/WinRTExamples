d9daa52019d17c384b61192de4f8a5faf618873e
jeremylikness_cp <SND\jeremylikness_cp>
2013-07-10 23:23:13 +0000

    Conversion of Chapter 1 to Windows 8.1

---
+++ WinRTByExample81/ExampleWinRTComponent/MyComponent.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MyComponent.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The uri loaded.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ExampleWinRTComponent
+{
+    using System;
+
+    /// <summary>
+    /// The uri loaded.
+    /// </summary>
+    /// <param name="sender">
+    /// The sender.
+    /// </param>
+    /// <param name="args">
+    /// The args.
+    /// </param>
+    public delegate void UriLoaded(object sender, UriLoadedArgs args);
+
+    /// <summary>
+    /// The my component.
+    /// </summary>
+    public sealed class MyComponent
+    {
+        /// <summary>
+        /// The uri loaded.
+        /// </summary>
+        public event UriLoaded UriLoaded;
+
+        /// <summary>
+        /// The uri to string.
+        /// </summary>
+        /// <param name="endPoint">
+        /// The end point.
+        /// </param>
+        public void UriToString(Uri endPoint)
+        {
+            var handler = this.UriLoaded;
+
+            if (handler != null)
+            {
+                handler(this, new UriLoadedArgs { Uri = endPoint.ToString() });
+            }
+        }
+    }
+}
