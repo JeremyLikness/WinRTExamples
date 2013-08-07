d1daba83db5e13b0179db65ba13b822e5e467a10
jeremylikness_cp <SND\jeremylikness_cp>
2013-08-07 23:44:49 +0000

    Porting animations example from Chapter 3 to Windows 8.1

---
+++ WinRTByExample81/AnimationsExample/BaseAnimationType.cs
@@ -0,0 +1,56 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="BaseAnimationType.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The base animation type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AnimationsExample
+{
+    using System.Threading;
+
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Media.Animation;
+
+    /// <summary>
+    /// The base animation type.
+    /// </summary>
+    public abstract class BaseAnimationType : IAnimationType
+    {
+        /// <summary>
+        /// The id.
+        /// </summary>
+        private static int id;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BaseAnimationType"/> class.
+        /// </summary>
+        protected BaseAnimationType()
+        {
+            this.Id = Interlocked.Add(ref id, 1);
+        }       
+
+        /// <summary>
+        /// Gets the id.
+        /// </summary>
+        public int Id { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public abstract string Description { get; protected set; }
+
+        /// <summary>
+        /// The start animation.
+        /// </summary>
+        /// <param name="target">
+        /// The target.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Storyboard"/>.
+        /// </returns>
+        public abstract Storyboard GenerateAnimation(FrameworkElement target);
+    }
+}
---
+++ WinRTByExample81/AnimationsExample/FadeOutThemeType.cs
@@ -0,0 +1,51 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="FadeOutThemeType.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The fade-out theme type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace AnimationsExample
+{
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Media.Animation;
+
+    /// <summary>
+    /// The bounce ease type.
+    /// </summary>
+    public class FadeOutThemeType : BaseAnimationType
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FadeOutThemeType"/> class. 
+        /// </summary>
+        public FadeOutThemeType()
+        {
+            this.Description = "Fade-out Theme";
+        }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public override sealed string Description { get; protected set; }
+
+        /// <summary>
+        /// The start animation.
+        /// </summary>
+        /// <param name="target">
+        /// The target.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Storyboard"/>.
+        /// </returns>
+        public override Storyboard GenerateAnimation(FrameworkElement target)
+        {
+            var fadeOutTheme = new FadeOutThemeAnimation();
+            Storyboard.SetTarget(fadeOutTheme, target);
+            var storyboard = new Storyboard();
+            storyboard.Children.Add(fadeOutTheme);
+            return storyboard;
+        }
+    }
+}
