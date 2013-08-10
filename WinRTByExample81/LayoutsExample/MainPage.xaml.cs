5e1edc4ec93822508b53da3564ee6d07c58aeeed
jeremylikness_cp <SND\jeremylikness_cp>
2013-08-10 18:52:07 +0000

    Convert LayoutExamples (Chapter 3) to Windows 8.1

---
+++ WinRTByExample81/LayoutsExample/MainPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MainPage.xaml.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   An empty page that can be used on its own or navigated to within a Frame.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LayoutsExample
+{
+    using System;
+
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Media;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainPage"/> class.
+        /// </summary>
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += this.MainPageLoaded;
+        }
+
+        /// <summary>
+        /// The main page loaded.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        public void MainPageLoaded(object sender, RoutedEventArgs e)
+        {
+            var viewModel = LayoutRoot.Resources["ViewModel"] as ViewModel;
+            
+            if (viewModel == null)
+            {
+                return;
+            }
+            viewModel.GoToVisualState = state => VisualStateManager.GoToState(this, state, true);
+            viewModel.GoToStretch =
+                stretch =>
+                {
+                    var newStretch = (Stretch)Enum.Parse(typeof(Stretch), stretch);
+                    this.ViewBoxInstance.SetValue(Viewbox.StretchProperty, newStretch);
+                };
+        }        
+    }
+}
---
+++ WinRTByExample81/LayoutsExample/ShapeType.cs
@@ -0,0 +1,37 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ShapeType.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The shape type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace LayoutsExample
+{
+    /// <summary>
+    /// The shape type.
+    /// </summary>
+    public enum ShapeType
+    {
+        /// <summary>
+        /// The circle.
+        /// </summary>
+        Circle,
+
+        /// <summary>
+        /// The square.
+        /// </summary>
+        Square,
+
+        /// <summary>
+        /// The ellipse
+        /// </summary>
+        Ellipse,
+
+        /// <summary>
+        /// The rectangle
+        /// </summary>
+        Rectangle
+    }
+}
