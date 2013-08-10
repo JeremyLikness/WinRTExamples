bc8579658acba71057ad147657e06851291549ab
Jeremy Likness <jeremy@jeremylikness.com>
2013-08-10 18:59:43 +0000

    Convert templates example (Chapter 3) to Windows 8.1

---
+++ WinRTByExample81/TemplatesExample/MessageType.cs
@@ -0,0 +1,32 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="MessageType.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   Type of the message
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace TemplatesExample
+{
+    /// <summary>
+    /// Type of the message
+    /// </summary>
+    public enum MessageType
+    {
+        /// <summary>
+        /// Message is information only.
+        /// </summary>
+        Information,
+
+        /// <summary>
+        /// Message is a warning.
+        /// </summary>
+        Warning,
+
+        /// <summary>
+        /// Message describes an error.
+        /// </summary>
+        Error
+    }
+}
