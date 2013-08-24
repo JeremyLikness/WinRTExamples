a5342c23a25521c2ee8819a9a1aa58426e07e127
Jeremy Likness <jeremy@jeremylikness.com>
2013-08-24 18:21:24 +0000

    Add flyout settings example for Chapter 3

--- WinRTByExample81/VisualStateExample/OrientationHandler.cs
+++ WinRTByExample81/VisualStateExample/OrientationHandler.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VisualStateExample
+﻿namespace VisualStateExample
 {
     using Windows.UI.ViewManagement;
     using Windows.UI.Xaml;
