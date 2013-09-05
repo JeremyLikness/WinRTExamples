a7aaaa6d48d4f0a93686b0e6a273f97f4e1bb887
jeremylikness_cp <SND\jeremylikness_cp>
2013-09-05 22:46:19 +0000

    Migrate Skrape to 8.1 for Chapter 4

---
+++ WinRTByExample81/Skrape/App.xaml.cs
@@ -0,0 +1,95 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   Provides application-specific behavior to supplement the default Application class.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Skrape
+{
+    using System;
+
+    using Data;
+
+    using Windows.ApplicationModel.Activation;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    public sealed partial class App
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class. 
+        /// </summary>
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the current resource.
+        /// </summary>
+        public GlobalViewModel CurrentViewModel
+        {
+            get
+            {
+                return (GlobalViewModel)Resources["GlobalResources"];
+            }
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used when the application is launched to open a specific file, to display
+        /// search results, and so forth.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs args)
+        {
+            var rootFrame = Window.Current.Content as Frame;
+
+            var dataManager = new SkrapeDataManager();
+            var webScraper = new WebScraper();
+            var pageManager = new PageAndGroupManager();
+            dataManager.Scraper = webScraper;
+            dataManager.Manager = pageManager;
+
+            CurrentViewModel.DataManager = dataManager;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                if (args.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                if (!rootFrame.Navigate(typeof(MainPage), args.Arguments))
+                {
+                    throw new Exception("Failed to create initial page");
+                }
+            }
+
+            CurrentViewModel.GoHome = () => rootFrame.Navigate(typeof(MainPage));
+
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+    }
+}
---
+++ WinRTByExample81/Skrape/Data/SkrapeDataManager.cs
@@ -0,0 +1,518 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SkrapeDataManager.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The data manager for scrapes.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace Skrape.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    using Common;
+    using Contracts;
+
+    using Windows.Storage;
+    using Windows.Storage.Pickers;
+    using Windows.UI.Core;
+    using Windows.UI.Popups;
+
+    /// <summary>
+    /// The data manager for scrapes.
+    /// </summary>
+    public class SkrapeDataManager : BasePropertyChange  
+    {
+        /// <summary>
+        /// The group id provider.
+        /// </summary>
+        private readonly IdProvider groupIdProvider = new IdProvider();
+
+        /// <summary>
+        /// The page id provider.
+        /// </summary>
+        private readonly IdProvider pageIdProvider = new IdProvider();
+
+        /// <summary>
+        /// The _groups.
+        /// </summary>
+        private readonly ObservableCollection<SkrapeGroup> groups = new ObservableCollection<SkrapeGroup>();
+
+        /// <summary>
+        /// The image types.
+        /// </summary>
+        private readonly string[] imageTypes =
+        {
+            "image/jpeg", "image/jpe", "image/jpg", "image/gif", "image/png",
+            "image/bmp", "image/tiff"
+        };
+
+        /// <summary>
+        /// The image extensions.
+        /// </summary>
+        private readonly string[] imageExtensions = { "jpg", "jpg", "jpg", "gif", "png", "bmp", "tif" };
+
+        /// <summary>
+        /// The current group.
+        /// </summary>
+        private SkrapeGroup currentGroup;
+
+        /// <summary>
+        /// The current page.
+        /// </summary>
+        private SkrapedPage currentPage;
+
+        /// <summary>
+        /// The current image
+        /// </summary>
+        private Uri currentImage;
+
+        /// <summary>
+        /// The current page and group manager
+        /// </summary>
+        private IPageAndGroupManager pageAndGroupManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SkrapeDataManager"/> class.
+        /// </summary>
+        public SkrapeDataManager()
+        {
+            if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
+            {
+                GenerateTestData();
+            }
+        }
+        
+        /// <summary>
+        /// Gets or sets the implementation of the web scraper
+        /// </summary>
+        public IWebScraper Scraper { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the implementation of the page and group manager
+        /// </summary>
+        public IPageAndGroupManager Manager
+        {
+            get
+            {
+                return pageAndGroupManager;
+            }
+
+            set
+            {
+                pageAndGroupManager = value;
+                if (pageAndGroupManager != null)
+                {
+                    pageAndGroupManager.NewUriAdded += PageAndGroupManagerOnNewUriAdded;
+                }   
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the dispatcher.
+        /// </summary>
+        public CoreDispatcher Dispatcher { get; set; }
+
+        /// <summary>
+        /// Gets the groups.
+        /// </summary>
+        public ObservableCollection<SkrapeGroup> Groups
+        {
+            get
+            {
+                return groups;
+            }
+        }
+        
+        /// <summary>
+        /// Gets or sets the current group.
+        /// </summary>
+        public SkrapeGroup CurrentGroup
+        {
+            get
+            {
+                return currentGroup;
+            }
+
+            set
+            {
+                currentGroup = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current page.
+        /// </summary>
+        public SkrapedPage CurrentPage
+        {
+            get
+            {
+                return currentPage;
+            }
+
+            set
+            {
+                currentPage = value;
+                OnPropertyChanged();
+                CurrentGroup = GetGroupForPage(value);
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the current image.
+        /// </summary>
+        public Uri CurrentImage
+        {
+            get
+            {
+                return currentImage;
+            }
+
+            set
+            {
+                currentImage = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// The initialize.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/> to run asynchronously.
+        /// </returns>
+        public async Task Initialize()
+        {
+            if (groups.Any())
+            {
+                return;
+            }
+            
+            foreach (var group in await Manager.RestoreGroups())
+            {
+                groups.Add(group);
+                groupIdProvider.RegisterId(group.Id);
+                foreach (var page in group.Pages)
+                {
+                    pageIdProvider.RegisterId(page.Id);
+                }
+            }
+
+            if (groups.Any())
+            {
+                return;
+            }
+
+            await
+                AddUrl(
+                    new Uri("http://csharperimage.jeremylikness.com/2013/02/review-of-lenovo-ideapad-yoga-13-for.html"));
+        }
+
+        /// <summary>
+        /// The get group for page.
+        /// </summary>
+        /// <param name="pageToCheck">
+        /// The page to check.
+        /// </param>
+        /// <returns>
+        /// The <see cref="SkrapeGroup"/>.
+        /// </returns>
+        public SkrapeGroup GetGroupForPage(SkrapedPage pageToCheck)
+        {
+            return groups.SelectMany(
+                @group => @group.Pages, 
+                (@group, page) => new { @group, page })
+                .Where(pageAndGroup => pageAndGroup.page.Id == pageToCheck.Id)
+                .Select(pageAndGroup => pageAndGroup.group)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// The add url.
+        /// </summary>
+        /// <param name="url">
+        /// The url.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/> to run asynchronously.
+        /// </returns>
+        public async Task AddUrl(Uri url)
+        {
+            var existingPage = groups.SelectMany(g => g.Pages, (g, p) => new { g, p })
+                                    .Where(@t => !@t.p.Deleted && @t.p.Url == url)
+                                    .Select(@t => @t.p).Any();
+
+            if (existingPage)
+            {
+                return;
+            }
+
+            var page = new SkrapedPage
+                           {
+                               Id = pageIdProvider.GetId(),
+                               Url = url,
+                               Title = "New Skrape - Tap to Load",
+                               ThumbnailPath = new Uri("ms-appx:///Assets/ie.png")
+                           };
+
+            var domain = url.Host.ToLower();
+            var group = groups.FirstOrDefault(g => g.Title == domain);
+
+            if (group != null)
+            {
+                group.Pages.Add(page);
+                await Manager.SaveGroup(group);
+            }
+            else
+            {
+                var newGroup = new SkrapeGroup
+                                   {
+                                       Id = groupIdProvider.GetId(),
+                                       Title = domain,
+                                       Description =
+                                           string.Format("Collection of Skrapes for the {0} domain.", domain)
+                                   };
+                newGroup.Pages.Add(page);
+                Groups.Add(newGroup);
+                await Manager.SaveGroup(newGroup);
+            }
+
+            await Manager.SavePage(page);
+
+            Manager.AddUri(page.Url);
+        }
+
+        /// <summary>
+        /// The delete page method
+        /// </summary>
+        /// <param name="page">
+        /// The page.
+        /// </param>
+        public void DeletePage(SkrapedPage page)
+        {
+            var parent = GetGroupForPage(page);
+            page.Deleted = true;
+            parent.Pages.Remove(page);
+
+            Manager.DeletePage(page);
+
+            if (parent.Pages.Any())
+            {
+                return;
+            }
+
+            groups.Remove(parent);
+            Manager.DeleteGroup(parent);
+        }
+        
+        /// <summary>
+        /// The process image routine
+        /// </summary>
+        /// <param name="imageUri">
+        /// The image uri.
+        /// </param>
+        /// <param name="id">
+        /// The id.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public async Task<Uri> ProcessImage(Uri imageUri, int id)
+        {
+            try
+            {
+                // this can be refactored to use the DownloadImage method
+                var client = new HttpClient();
+                var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, imageUri));
+                response.EnsureSuccessStatusCode();
+                var buffer = await response.Content.ReadAsByteArrayAsync();
+                var type = response.Content.Headers.ContentType;
+                var idx = Array.IndexOf(imageTypes, type.MediaType);
+                string extension;
+                if (idx >= 0)
+                {
+                    extension = imageExtensions[idx];
+                }
+                else
+                {
+                    return null;
+                }
+
+                var file = string.Format("{0}.{1}", id, extension);
+                var path = string.Format("ms-appdata:///local/Images/{0}", file);
+                var folder =
+                    await
+                    ApplicationData.Current.LocalFolder.CreateFolderAsync(
+                    "Images", 
+                    CreationCollisionOption.OpenIfExists);
+                var storageFile = await folder.CreateFileAsync(file, CreationCollisionOption.ReplaceExisting);
+                await FileIO.WriteBytesAsync(storageFile, buffer);
+                return new Uri(path);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// The save image routine
+        /// </summary>
+        /// <param name="imageUri">
+        /// The image uri.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public async Task SaveImage(Uri imageUri)
+        {
+            var result = await DownloadImage(imageUri);
+
+            if (string.IsNullOrEmpty(result.Extension))
+            {
+                return;
+            }
+
+            var extension = string.Format(".{0}", result.Extension);
+
+            var saveFilePicker = 
+                new FileSavePicker
+                    {
+                        SuggestedFileName = "WebImage",
+                        SuggestedStartLocation = PickerLocationId.PicturesLibrary,
+                        DefaultFileExtension = extension
+                    };
+            
+            saveFilePicker.FileTypeChoices.Add(
+                "Image File", new List<string>(new[] { extension }));
+
+            var storageFile = await saveFilePicker.PickSaveFileAsync();
+
+            if (storageFile == null)
+            {
+                return;
+            }
+
+            CachedFileManager.DeferUpdates(storageFile);
+            await FileIO.WriteBytesAsync(storageFile, result.Buffer);
+            await CachedFileManager.CompleteUpdatesAsync(storageFile);
+        }
+
+        /// <summary>
+        /// The download image method.
+        /// </summary>
+        /// <param name="imageUri">
+        /// The image uri.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Task"/> to return asynchronously.
+        /// </returns>
+        private async Task<ImageDownloadResult> DownloadImage(Uri imageUri)
+        {
+            var result = new ImageDownloadResult();
+            var client = new HttpClient();
+            var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, imageUri));
+            response.EnsureSuccessStatusCode();
+            result.Buffer = await response.Content.ReadAsByteArrayAsync();
+            var type = response.Content.Headers.ContentType;
+            var idx = Array.IndexOf(imageTypes, type.MediaType);
+            if (idx >= 0)
+            {
+                result.Extension = imageExtensions[idx];
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// The page and group manager on current page changed.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="newUri">
+        /// The new uri
+        /// </param>
+        private async void PageAndGroupManagerOnNewUriAdded(object sender, Uri newUri)
+        {
+            var page = (from g in groups from p in g.Pages where p.Url == newUri select p).FirstOrDefault();
+
+            if (page != null)
+            {
+                return;
+            }
+
+            await Dispatcher.RunAsync(
+                CoreDispatcherPriority.Normal,
+                async () =>
+                    {
+                        var dialog = new MessageDialog(
+                            "A new URL has been entered on another device. Exit the program and re-launch to synchronize.", newUri.ToString());
+                        await dialog.ShowAsync();
+                    });
+        }
+
+        /// <summary>
+        /// The generate test data method.
+        /// </summary>
+        private void GenerateTestData()
+        {
+            var baseuri = new Uri("http://csharperimage.jeremylikness.com/", UriKind.Absolute);
+            var groupNames = new[] { "Alpha", "Beta", "Omega " };
+            foreach (var groupName in groupNames)
+            {
+                var pages = new[]
+                                {
+                                    "/2013/02/review-of-lenovo-ideapad-yoga-13-for.html",
+                                    "/2010/04/model-view-viewmodel-mvvm-explained.html",
+                                    "/2010/10/so-whats-fuss-about-silverlight.html",
+                                    "/2013/01/traveling-with-microsoft-and-asus.html"
+                                };
+                var images = new[]
+                                 {
+                                     new Uri(
+                                         "http://1.bp.blogspot.com/-R7LqKNm9BBk/Tl4oD5P9JdI/AAAAAAAAAZ8/pZ8J8gz_e5E/s1600/BlogHeader4.png"),                                     
+                                     new Uri(
+                                         "http://lh6.ggpht.com/-YPUxFruIDFA/USFp0raqHAI/AAAAAAAAA6E/bHy3vg05Ed4/picture005_thumb2.jpg?imgmax=800"),                                     
+                                     new Uri(
+                                         "http://lh6.ggpht.com/-WYkLA5K30mw/UObJc7Ls6bI/AAAAAAAAA3A/2gng-TfN6aE/WP_000232_thumb%25255B1%25255D.jpg?imgmax=800")
+                                 };
+
+                var group = new SkrapeGroup();
+                group.Id = groupIdProvider.GetId();
+                group.Title = groupName;
+                group.Description =
+                    string.Format(
+                        "The {0} group for holding a set of test pages for the sample data of the application.",
+                        groupName);
+                for (var x = 0; x < 50; x++)
+                {
+                    var pageName = pages[x % pages.Length];
+                    var pageUri = new Uri(baseuri, pageName);
+                    var page = new SkrapedPage
+                                   {
+                                       Id = pageIdProvider.GetId(), 
+                                       Url = pageUri,
+                                       Title = pageName.Replace("/", " "),
+                                       ThumbnailPath = new Uri("ms-appx:///Assets/Logo.png")
+                                   };
+                    foreach (var image in images)
+                    {
+                        page.Images.Add(image);
+                    }
+
+                    group.Pages.Add(page);                    
+                }
+
+                groups.Add(group);                
+            }            
+        }        
+    }
+}
