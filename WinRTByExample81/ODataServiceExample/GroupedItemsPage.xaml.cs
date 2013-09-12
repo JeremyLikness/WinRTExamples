a6dae8ff265fcf6a1cb93734a8e6887b2693e5e2
Jeremy Likness <jeremy@jeremylikness.com>
2013-09-12 21:39:54 +0000

    Convert OData services example to 8.1 for Chapter 5

---
+++ WinRTByExample81/ODataServiceExample/GroupedItemsPage.xaml.cs
@@ -0,0 +1,110 @@
+﻿using ODataServiceExample.Common;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using ODataServiceExample.DataModel;
+
+namespace ODataServiceExample
+{
+    /// <summary>
+    /// A page that displays a grouped collection of items.
+    /// </summary>
+    public sealed partial class GroupedItemsPage
+    {
+        private readonly NavigationHelper navigationHelper;
+        private readonly ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        public GroupedItemsPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            var dataSource = ((App)Application.Current).DataSource;
+            DefaultViewModel["Groups"] = dataSource.Categories; 
+        }
+
+        /// <summary>
+        /// Invoked when a group header is clicked.
+        /// </summary>
+        /// <param name="sender">The Button used as a group header for the selected group.</param>
+        /// <param name="e">Event data that describes how the click was initiated.</param>
+        void HeaderClick(object sender, RoutedEventArgs e)
+        {
+            var frameworkElement = sender as FrameworkElement;
+            if (frameworkElement == null)
+            {
+                return;
+            }
+
+            var category = frameworkElement.DataContext;
+            this.Frame.Navigate(typeof (GroupDetailPage), ((Category) category).Id);
+        }
+
+        /// <summary>
+        /// Invoked when an item within a group is clicked.
+        /// </summary>
+        /// <param name="sender">The GridView (or ListView when the application is snapped)
+        /// displaying the item clicked.</param>
+        /// <param name="e">Event data that describes the item clicked.</param>
+        void ItemViewItemClick(object sender, ItemClickEventArgs e)
+        {
+            var itemId = ((Product)e.ClickedItem).Id;
+            Frame.Navigate(typeof(ItemDetailPage), itemId);
+        }
+
+        #region NavigationHelper registration
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (this.Frame.CanGoBack)
+            {
+                this.Frame.GoBack();
+            }
+        }
+    }
+}
---
+++ WinRTByExample81/ODataServiceExample/ItemDetailPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using ODataServiceExample.Common;
+using System;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace ODataServiceExample
+{
+    /// <summary>
+    /// A page that displays details for a single item within a group.
+    /// </summary>
+    public sealed partial class ItemDetailPage
+    {
+        private readonly NavigationHelper navigationHelper;
+        private readonly ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        public ItemDetailPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            var itemId = (int)e.NavigationParameter;
+            var item =
+                (from c in ((App)Application.Current).DataSource.Categories
+                 from p in c.Products
+                 where p.Id == itemId
+                 select new { p, c }).FirstOrDefault();
+            this.DefaultViewModel["Category"] = item.c;
+            this.DefaultViewModel["Products"] = item.c.Products;
+            this.flipView.SelectedItem = item.p;
+        }
+
+        #region NavigationHelper registration
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void BackButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+            {
+                Frame.GoBack();
+            }
+        }
+    }
+}
