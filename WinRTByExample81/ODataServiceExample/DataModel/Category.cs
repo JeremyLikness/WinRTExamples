d5ea4e1485f824838cf03a8fbf9302200fe199c8
jeremylikness_cp <SND\jeremylikness_cp>
2013-09-12 21:39:54 +0000

    Convert OData services example to 8.1 for Chapter 5

---
+++ WinRTByExample81/ODataServiceExample/DataModel/Category.cs
@@ -0,0 +1,53 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Category.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The category.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ODataServiceExample.DataModel
+{
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+
+    /// <summary>
+    /// The category.
+    /// </summary>
+    public class Category : BaseItem
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Category"/> class.
+        /// </summary>
+        public Category()
+        {
+            Products = new ObservableCollection<Product>();
+// ReSharper disable ExplicitCallerInfoArgument
+            Products.CollectionChanged += (o, e) => OnPropertyChanged("TopProducts");
+// ReSharper restore ExplicitCallerInfoArgument
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the list of products for this category
+        /// </summary>
+        public ObservableCollection<Product> Products { get; private set; }
+
+        /// <summary>
+        /// Gets the top products.
+        /// </summary>
+        public IEnumerable<Product> TopProducts
+        {
+            get
+            {
+                return Products.OrderBy(p => p.Title).Take(12);
+            }
+        }
+    }
+}
