ca5e33a7f90e00814a2aa2d264dd287bf8416b9e
jeremylikness_cp <SND\jeremylikness_cp>
2013-09-14 16:39:41 +0000

    Migrate SOAP Service example to Windows 8.1 for Chapter 5

---
+++ WinRTByExample81/SoapServiceExample/Common/NotifyBase.cs
@@ -0,0 +1,36 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NotifyBase.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The notify base.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SoapServiceExample.Common
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    /// <summary>
+    /// The notify base.
+    /// </summary>
+    public class NotifyBase : INotifyPropertyChanged
+    {
+        /// <summary>
+        /// The property changed event.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        /// <summary>
+        /// The on property changed method.
+        /// </summary>
+        /// <param name="propertyName">
+        /// The property name.
+        /// </param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {                        
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ WinRTByExample81/SoapServiceExample/WeatherCommand.cs
@@ -0,0 +1,85 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="WeatherCommand.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The weather command.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SoapServiceExample
+{
+    using System;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// The weather command.
+    /// </summary>
+    public class WeatherCommand : ICommand 
+    {
+        /// <summary>
+        /// The can execute changed.
+        /// </summary>
+        public event EventHandler CanExecuteChanged = delegate { };
+
+        /// <summary>
+        /// Gets or sets the execution action
+        /// </summary>
+        public Action<string> ExecuteAction { get; set; }
+
+        /// <summary>
+        /// The can execute.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool CanExecute(object parameter)
+        {
+            if (this.ExecuteAction == null)
+            {
+                return false;
+            }
+
+            var value = parameter as string;
+            
+            if (value == null)
+            {
+                return false;
+            }
+
+            if (value.Length != 5)
+            {
+                return false;
+            }
+
+            int zipCode;
+
+            return int.TryParse(value, out zipCode);
+        }
+
+        /// <summary>
+        /// The execute.
+        /// </summary>
+        /// <param name="parameter">
+        /// The parameter.
+        /// </param>
+        public void Execute(object parameter)
+        {
+            if (this.CanExecute(parameter))
+            {
+                this.ExecuteAction(parameter.ToString());
+            }
+        }
+        
+        /// <summary>
+        /// The on can execute change.
+        /// </summary>
+        public void OnCanExecuteChange()
+        {
+            this.CanExecuteChanged(this, EventArgs.Empty);
+        }
+    }
+}
