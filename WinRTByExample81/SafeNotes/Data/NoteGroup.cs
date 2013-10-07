a320548acd4d486ed5fe7b68deb50b6d89f074ba
jeremylikness_cp <SND\jeremylikness_cp>
2013-10-07 23:32:30 +0000

    Port Safe Notes to Windows 8.1 for Chapter 8

---
+++ WinRTByExample81/SafeNotes/Data/NoteGroup.cs
@@ -0,0 +1,87 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="NoteGroup.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The note group.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace SafeNotes.Data
+{
+    using System;
+    using System.Collections.ObjectModel;
+
+    /// <summary>
+    /// The note group.
+    /// </summary>
+    public class NoteGroup
+    {
+        /// <summary>
+        /// The start date range this group is for 
+        /// </summary>
+        private readonly DateTime startDate;
+
+        /// <summary>
+        /// The end date range this group is for 
+        /// </summary>
+        private readonly DateTime endDate; 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NoteGroup"/> class.
+        /// </summary>
+        /// <param name="startDate">
+        /// The start Date.
+        /// </param>
+        /// <param name="endDate">
+        /// The end Date.
+        /// </param>
+        public NoteGroup(DateTime startDate, DateTime endDate)
+        {
+            this.Notes = new ObservableCollection<SimpleNote>();
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the notes.
+        /// </summary>
+        public ObservableCollection<SimpleNote> Notes { get; private set; }
+
+        /// <summary>
+        /// The try add note.
+        /// </summary>
+        /// <param name="note">
+        /// The note.
+        /// </param>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
+        public bool TryAddNote(SimpleNote note)
+        {
+            if (note.DateModified >= this.startDate && note.DateModified < this.endDate)
+            {
+                this.Notes.Add(note);
+                return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// The to string.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="string"/>.
+        /// </returns>
+        public override string ToString()
+        {
+            return string.Format("{0} ({1} Notes)", this.Name, this.Notes.Count);
+        }
+    }
+}
---
+++ WinRTByExample81/SafeNotes/MainPage.xaml.cs
@@ -0,0 +1,123 @@
+﻿using Windows.UI.Xaml.Navigation;
+using SafeNotes.Common;
+using SafeNotes.Data;
+
+namespace SafeNotes
+{
+    using System;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        private NavigationHelper navigationHelper;
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            var viewModel = ((App)Application.Current).CurrentViewModel;
+            if (viewModel.IsInitialized)
+            {
+                return;
+            }
+
+            VisualStateManager.GoToState(this, "LoadingState", false);
+            await viewModel.InitializeAsync();
+            VisualStateManager.GoToState(this, "LoadedState", false);
+        }
+
+        private void MainGrid_OnItemClick(object sender, ItemClickEventArgs e)
+        {
+            var viewModel = ((App)Application.Current).CurrentViewModel;
+            viewModel.CurrentNote = e.ClickedItem as SimpleNote;
+            viewModel.SetEdit();
+            this.Frame.Navigate(typeof(NotePage), viewModel.CurrentNote.Id);
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation. Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session. The state will be null the first time a page is visited.</param>
+        private async void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+            ((App)Application.Current).CurrentViewModel.GoHome = () =>
+            {
+                if (this.Frame.CanGoBack)
+                {
+                    this.Frame.GoBack();
+                }
+                else
+                {
+                    this.Frame.Navigate(typeof(MainPage));
+                }
+            };
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var viewModel = ((App)Application.Current).CurrentViewModel;
+            viewModel.SetNew();
+            this.Frame.Navigate(typeof(NotePage), string.Empty);
+        }
+    }
+}
