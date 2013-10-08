8a3e6f26369986d46431ab134c3c2ce8ae1c50e8
jeremylikness_cp <SND\jeremylikness_cp>
2013-10-08 22:46:20 +0000

    Encryption example port to Windows 8.1 for Chapter 8

---
+++ WinRTByExample81/EncryptionExample/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace EncryptionExample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
