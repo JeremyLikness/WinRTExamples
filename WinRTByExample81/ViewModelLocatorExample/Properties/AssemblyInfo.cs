542a7814b63d55dd5063121c268b2c3231a66752
jeremylikness_cp <SND\jeremylikness_cp>
2013-10-13 18:20:37 +0000

    Updates to migrate MVVM chapter (Chapter 9) to Windows 8.1

---
+++ WinRTByExample81/ViewModelLocatorExample/Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ViewModelLocatorExample")]
+[assembly: AssemblyDescription("Example of using the view model locator pattern for MVVM.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("ViewModelLocatorExample")]
+[assembly: AssemblyCulture("")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
