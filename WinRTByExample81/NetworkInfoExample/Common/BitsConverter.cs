2e21182c0b9042ecff00a06184beb8cdd6fdbc02
jeremylikness_cp <SND\jeremylikness_cp>
2013-10-24 23:18:16 +0000

    Preliminary scaffold for network information example

---
+++ WinRTByExample81/NetworkInfoExample/Common/BitsConverter.cs
@@ -0,0 +1,56 @@
+﻿namespace NetworkInfoExample.Common
+{
+    using System;
+
+    using Windows.UI.Xaml.Data;
+
+    public class BitsConverter : IValueConverter
+    {
+        private const long Kilobit = 1024;
+
+        private const long Megabit = Kilobit * Kilobit;
+
+        private const long Gigabit = Megabit * Kilobit;
+
+        private const string Nothing = "0 bps";
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+            {
+                return Nothing;
+            }
+
+            var val = value.ToString();
+
+            long bits;
+
+            if (!long.TryParse(val, out bits))
+            {
+                return Nothing;
+            }
+
+            if (bits < Kilobit)
+            {
+                return string.Format("{0} bps", bits);
+            }
+                
+            if (bits < Megabit)
+            {
+                return string.Format("{0} kbps", bits / Kilobit);
+            }
+
+            if (bits < Gigabit)
+            {
+                return string.Format("{0} mbps", bits / Megabit);
+            }
+
+            return string.Format("{0} gbps", bits / Gigabit);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
