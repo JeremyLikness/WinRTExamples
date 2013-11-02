89ba80e6c61ae05390dcc89b40bf7b4ed4577974
Jeremy Likness <jeremy@jeremylikness.com>
2013-11-02 20:51:24 +0000

    Home group example for Chapter 10

--- WinRTByExample81/HomeGroupExample/Data/ViewModel.cs
+++ WinRTByExample81/HomeGroupExample/Data/ViewModel.cs
@@ -7,6 +7,12 @@
     using System.Runtime.CompilerServices;
     using System.Threading.Tasks;
 
+    using Windows.Storage.FileProperties;
+    using Windows.Storage.Search;
+    using Windows.UI.Xaml.Media.Imaging;
+
+    using HomeGroupExample.Common;
+
     public class ViewModel : INotifyPropertyChanged
     {
         private HomeGroupUser selectedUser;
@@ -14,6 +20,8 @@
         public ViewModel()
         {
             Users = new ObservableCollection<HomeGroupUser>();
+            Images = new ObservableCollection<ImageItem>();
+            PickUser = new ActionCommand(async obj => await SelectForUser(obj), IsValidUser);
 
             if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
             {
@@ -45,16 +53,60 @@
             }
         }
 
+        private async Task SelectForUser(object user)
+        {
+            var homeGroupUser = user as HomeGroupUser;
+            if (homeGroupUser != null)
+            {
+                var targetFolder =
+                    (await Windows.Storage.KnownFolders.HomeGroup.GetFoldersAsync()).FirstOrDefault(
+                        folder => folder.DisplayName == homeGroupUser.UserName);
+                if (targetFolder != null)
+                {
+                    var query = new QueryOptions(CommonFileQuery.OrderBySearchRank, new[] { ".jpg", ".png", ".bmp", ".gif" })
+                                    {
+                                        UserSearchFilter =
+                                            "kind:picture"
+                                    };
+                    var files = await targetFolder.CreateFileQueryWithOptions(query).GetFilesAsync();
+                    foreach (var file in files)
+                    {
+                        var image = new ImageItem { Name = file.DisplayName };
+                        var thumbnail = await file.GetThumbnailAsync(ThumbnailMode.PicturesView, 200);
+                        image.Image = new BitmapImage();
+                        image.Image.SetSource(thumbnail);
+                        Images.Add(image);
+                    }
+                }
+            }
+        }
+
+        private bool IsValidUser(object user)
+        {
+            var homeGroupUser = user as HomeGroupUser;
+            return homeGroupUser != null && homeGroupUser.IsHomeGroupUser;
+        }
+
         private void PopulateDesignData()
         {
             var user1 = new HomeGroupUser { UserName = "John Garland", IsHomeGroupUser = true };
             var user2 = new HomeGroupUser { UserName = "Jeremy Likness", IsHomeGroupUser = true };
             Users.Add(user1);
             Users.Add(user2);
-            SelectedUser = user1; 
+            SelectedUser = user1;
+            var image = new ImageItem
+                            {
+                                Name = "Sample Image",
+                                Image = new BitmapImage(new Uri("ms-appx:///Assets/Logo.scale-100.png"))
+                            };
+            Images.Add(image);
         }
 
         public ObservableCollection<HomeGroupUser> Users { get; set; }
+
+        public ObservableCollection<ImageItem> Images { get; set; } 
+
+        public ActionCommand PickUser { get; private set; }
 
         public HomeGroupUser SelectedUser
         {
