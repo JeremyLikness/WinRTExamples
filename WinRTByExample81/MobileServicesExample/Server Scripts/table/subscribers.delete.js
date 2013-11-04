89a7408cf66c12a8a41860f5b620193c8350954d
John Garland <dotnetgator@users.noreply.github.com>
2013-11-04 00:51:14 +0000

    Updates to synchronize server script content

--- WinRTByExample81/MobileServicesExample/Server Scripts/table/subscribers.delete.js
+++ WinRTByExample81/MobileServicesExample/Server Scripts/table/subscribers.delete.js
@@ -4,17 +4,21 @@ function del(id, user, request) {
     /// <param name="id" type="Number"></param>
     /// <param name="user" type="User"></param>
     /// <param name="request" type="Request"></param>
-    
+
     var subscriberTable = tables.current;
     subscriberTable.where({
         id: id
     }).read({
         success: function (items) {
             if (items.length === 0) {
-                request.respond(statusCodes.NOT_FOUND, { error: 'No matching item found to delete.' });
+                request.respond(
+                    statusCodes.NOT_FOUND,
+                    { error: 'No matching item found to delete.' });
             } else {
                 if (items[0].ownerId != user.userId) {
-                    request.respond(statusCodes.BAD_REQUEST, { error: "Cannot delete someone else's item." });
+                    request.respond(
+                        statusCodes.BAD_REQUEST,
+                        { error: "Cannot delete someone else's item." });
                 } else {
                     request.execute();
                 }
