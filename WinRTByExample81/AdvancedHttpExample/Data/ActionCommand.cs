087acd53632f6c85772b443120455ed045d7410a
Jeremy Likness <jeremy@jeremylikness.com>
2013-11-05 01:21:48 +0000

    Initial pass at advanced HTTP example

---
+++ WinRTByExample81/AdvancedHttpExample/Data/ActionCommand.cs
@@ -0,0 +1,48 @@
+﻿namespace AdvancedHttpExample.Data
+{
+    using System;
+    using System.Windows.Input;
+
+    public class ActionCommand : ICommand
+    {
+        private readonly Action<object> action = obj => { };
+
+        private readonly Predicate<object> permission = obj => true;
+
+        public ActionCommand()
+        {
+
+        }
+
+        public ActionCommand(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public ActionCommand(Action<object> action, Predicate<object> permission)
+        {
+            this.action = action;
+            this.permission = permission;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.permission(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            if (this.CanExecute(parameter))
+            {
+                this.action(parameter);
+            }
+        }
+
+        public void OnCanExecuteChanged()
+        {
+            this.CanExecuteChanged(this, EventArgs.Empty);
+        }
+
+        public event EventHandler CanExecuteChanged = delegate { };
+    }
+}
