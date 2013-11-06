5572a470f5042b725f2ce0aeff52f17720bb0ca8
Jeremy Likness <jeremy@jeremylikness.com>
2013-11-06 00:08:13 +0000

    Advanced HTTP sample for chapter 10

--- WinRTByExample81/AdvancedHttpExample/Data/ViewModel.cs
+++ WinRTByExample81/AdvancedHttpExample/Data/ViewModel.cs
@@ -13,36 +13,28 @@
     {
         public ViewModel()
         {
-            this.Url = "http://csharperimage.jeremylikness.com/";
+            this.Url = "http://csharperimage.jeremylikness.com/"; // http://www.gutenberg.org/files/4300/4300-h/4300-h.htm
 
             this.LoadUrlCommand = new ActionCommand(obj => this.LoadUrl(), obj => this.CanLoadUrl());
             this.CancelUrlCommand = new ActionCommand(obj => this.CancelUrl(), obj => this.CanCancelUrl());
 
             if (Windows.ApplicationModel.DesignMode.DesignModeEnabled)
             {
-                this.BytesSent = 1000;
-                this.BytesToSend = 10000;
                 this.BytesReceived = 500;
-                this.BytesToReceive = 1000;
                 this.Content = "Blah blah blah.";
             }
             else
             {
                 this.dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+                this.client = new HttpClient();
             }
         }
 
         private readonly CoreDispatcher dispatcher;
 
-        private IProgress<HttpProgress> progress; 
-
-        private ulong bytesSent;
-
-        private ulong bytesToSend; 
+        private IProgress<ulong> progress; 
 
         private ulong bytesReceived;
-
-        private ulong bytesToReceive;
 
         private string url;
 
@@ -50,39 +42,13 @@
 
         private bool loading;
 
-        private HttpClient client;
+        private readonly HttpClient client;
 
         private CancellationTokenSource cancellation;
 
         public ActionCommand LoadUrlCommand { get; private set; }
 
         public ActionCommand CancelUrlCommand { get; private set; }
-
-        public ulong BytesSent
-        {
-            get
-            {
-                return this.bytesSent;
-            }
-            set
-            {
-                this.bytesSent = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        public ulong BytesToSend
-        {
-            get
-            {
-                return this.bytesToSend;
-            }
-            set
-            {
-                this.bytesToSend = value;
-                this.OnPropertyChanged();
-            }
-        }
 
         public ulong BytesReceived
         {
@@ -93,19 +59,6 @@
             set
             {
                 this.bytesReceived = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        public ulong BytesToReceive
-        {
-            get
-            {
-                return this.bytesToReceive;
-            }
-            set
-            {
-                this.bytesToReceive = value;
                 this.OnPropertyChanged();
             }
         }
@@ -157,22 +110,27 @@
                 Uri uri;
                 if (Uri.TryCreate(this.Url, UriKind.Absolute, out uri))
                 {
-                    this.client = new HttpClient();
+                    this.Content = "{{ loading }}";
                     this.cancellation = new CancellationTokenSource();
                     var request = new HttpRequestMessage(HttpMethod.Get, uri);
-                    this.progress = new Progress<HttpProgress>(ProgressHandler);
                     this.loading = true;
                     this.LoadUrlCommand.OnCanExecuteChanged();
                     this.CancelUrlCommand.OnCanExecuteChanged();
-                    var response = await client.SendRequestAsync(request).AsTask(cancellation.Token, progress);
-                    if (!cancellation.IsCancellationRequested)
+                    var response = await client.SendRequestAsync(request, HttpCompletionOption.ResponseHeadersRead).AsTask(cancellation.Token);
+                    if (!cancellation.IsCancellationRequested && response.IsSuccessStatusCode)
                     {
-                        this.Content = await response.Content.ReadAsStringAsync();
+                        cancellation.Dispose();
+                        cancellation = new CancellationTokenSource();
+                        this.progress = new Progress<ulong>(ProgressHandler);
+                        var stringContent = await response.Content.ReadAsStringAsync().AsTask(cancellation.Token, this.progress);
+                        if (stringContent.Length > 10000)
+                        {
+                            stringContent = stringContent.Substring(0, 10000);
+                        }
+                        this.Content = stringContent;
                         this.loading = false;
                         this.cancellation.Dispose();
                         this.cancellation = null;
-                        this.client.Dispose();
-                        this.client = null;
                         this.LoadUrlCommand.OnCanExecuteChanged();
                         this.CancelUrlCommand.OnCanExecuteChanged();
                         this.progress = null;
@@ -181,14 +139,11 @@
             }
         }
 
-        private void ProgressHandler(HttpProgress progressArgs)
+        private void ProgressHandler(ulong progressArgs)
         {
-            Action<HttpProgress> update = p =>
+            Action<ulong> update = p =>
                 {
-                    this.bytesReceived = p.BytesReceived;
-                    this.bytesToReceive = p.TotalBytesToReceive ?? 0;
-                    this.bytesSent = p.BytesSent;
-                    this.bytesToSend = p.TotalBytesToSend ?? 0;
+                    this.BytesReceived = p;                    
                 };
 
             if (this.dispatcher.HasThreadAccess)
@@ -212,12 +167,11 @@
             {
                 this.cancellation.Cancel();
                 this.cancellation.Dispose();
-                this.client.Dispose();
-                this.client = null;
                 this.loading = false;
                 this.cancellation = null;
                 this.progress = null;
                 this.Content = "Cancelled.";
+                this.BytesReceived = 0;
                 this.LoadUrlCommand.OnCanExecuteChanged();
                 this.CancelUrlCommand.OnCanExecuteChanged();
             }
