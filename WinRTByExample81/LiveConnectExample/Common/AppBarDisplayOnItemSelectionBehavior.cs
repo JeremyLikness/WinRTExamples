89ab1ce72392f53917ed0a8cc0a77b1509fef5ce
John Garland <dotnetgator@users.noreply.github.com>
2013-11-08 01:56:54 +0000

    Updated to include behaviors for app bar vs selector controls

---
+++ WinRTByExample81/LiveConnectExample/Common/AppBarDisplayOnItemSelectionBehavior.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Linq;
+using Windows.ApplicationModel;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Microsoft.Xaml.Interactivity;
+
+namespace LiveConnectExample
+{
+    public class AppBarDisplayOnItemSelectionBehavior : DependencyObject, IBehavior
+    {
+        /// <summary>
+        /// Attaches to the specified object.
+        /// </summary>
+        /// <param name="associatedObject">The <see cref="T:Windows.UI.Xaml.DependencyObject"/> to which the <seealso cref="T:Microsoft.Xaml.Interactivity.IBehavior"/> will be attached.</param>
+        public void Attach(DependencyObject associatedObject)
+        {
+            if ((associatedObject != AssociatedObject) && !DesignMode.DesignModeEnabled)
+            {
+                if (AssociatedObject != null) throw new InvalidOperationException("Cannot attach behavior multiple times.");
+                AssociatedObject = associatedObject;
+                var selector = AssociatedObject as Selector;
+                if (selector != null)
+                {
+                    HookEvents(selector);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Detaches this instance from its associated object.
+        /// </summary>
+        public void Detach()
+        {
+            var selector = AssociatedObject as Selector;
+            if (selector != null)
+            {
+                UnhookEvents(selector);
+            }
+        }
+
+        /// <summary>
+        /// Gets the <see cref="T:Windows.UI.Xaml.DependencyObject"/> to which the <seealso cref="T:Microsoft.Xaml.Interactivity.IBehavior"/> is attached.
+        /// </summary>
+        public DependencyObject AssociatedObject
+        {
+            get; private set;
+        }
+
+        public AppBarDisplayFlags DisplayOnSelectionAction { get; set; }
+
+
+        private void HookEvents(Selector selector)
+        {
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            // Clear any "active" event handlers  
+            UnhookEvents(selector);
+
+            if (DisplayOnSelectionAction != AppBarDisplayFlags.None)
+            {
+                selector.Unloaded += HandleUnloaded;
+                selector.SelectionChanged += HandleSelectionChanged;
+            }
+        }
+
+        private void UnhookEvents(Selector selector)
+        {
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            selector.Unloaded -= HandleUnloaded;
+            selector.SelectionChanged -= HandleSelectionChanged;
+        }
+
+        private void HandleUnloaded(Object sender, RoutedEventArgs e)
+        {
+            UnhookEvents((Selector)sender);
+        }
+
+        private void HandleSelectionChanged(Object sender, SelectionChangedEventArgs e)
+        {
+            var selector = sender as Selector;
+            if (selector == null) return;
+            // Traverse the selector's parents to find the first "page" element  
+            var containingPage = selector.GetVisualAncestors().OfType<Page>().FirstOrDefault();
+            if (containingPage == null) return;
+
+
+            var currentFlags = DisplayOnSelectionAction;
+            var showBottomAppBar = (currentFlags & AppBarDisplayFlags.Bottom) == AppBarDisplayFlags.Bottom;
+            var showTopAppBar = (currentFlags & AppBarDisplayFlags.Top) == AppBarDisplayFlags.Top;
+            if (selector.SelectedItem != null)
+            {
+                // An item has been selected - show the relevant app bars  
+                if (showBottomAppBar) ShowAppBar(containingPage.BottomAppBar);
+                if (showTopAppBar) ShowAppBar(containingPage.TopAppBar);
+            }
+            else
+            {
+                // Nothing has been selected - hide the relevant app bars  
+                if (showBottomAppBar) HideAppBar(containingPage.BottomAppBar);
+                if (showTopAppBar) HideAppBar(containingPage.TopAppBar);
+            }
+        }
+
+        private void ShowAppBar(AppBar appBar)
+        {
+            if (appBar == null) return;
+
+            appBar.IsSticky = true;
+            appBar.IsOpen = true;
+        }
+
+        private void HideAppBar(AppBar appBar)
+        {
+            if (appBar == null) return;
+
+            appBar.IsOpen = false;
+            appBar.IsSticky = false;
+        }
+
+        [Flags]
+        public enum AppBarDisplayFlags
+        {
+            None = 0,
+            Bottom = 1,
+            Top = 2,
+            BottomAndTop = 3,
+        }
+    }
+}
---
+++ WinRTByExample81/LiveConnectExample/Common/VisualTreeHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Media;
+
+namespace LiveConnectExample
+{
+    public static class VisualTreeHelpers
+    {
+        /// <summary> 
+        /// Gets the ancestors of the element, up to the root. 114:     
+        /// </summary> 115:     
+        /// <param name="node">The element to start from.</param> 116:     
+        /// <returns>An enumerator of the ancestors.</returns> 117:     
+        public static IEnumerable<FrameworkElement> GetVisualAncestors(this FrameworkElement node)
+        {
+            var parent = node.GetVisualParent();
+            while (parent != null)
+            {
+                yield return parent;
+                parent = parent.GetVisualParent();
+            }
+        }
+
+        /// <summary> 
+        /// Gets the visual parent of the element.
+        /// </summary>  
+        /// <param name="node">The element to check.</param> 131:     
+        /// /// <returns>The visual parent.</returns> 132:     
+        public static FrameworkElement GetVisualParent(this FrameworkElement node)
+        {
+            return VisualTreeHelper.GetParent(node) as FrameworkElement;
+        }
+    }
+}
