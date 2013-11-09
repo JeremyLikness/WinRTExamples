fa78db8e3f711a3d6da9d436258af66e3e9b5ca3
avidgator_cp <SND\avidgator_cp>
2013-11-09 02:55:05 +0000

    Code cleanup, added ui lablel in skydrive items page

---
+++ WinRTByExample81/LiveConnectExample/SkyDrive/SkyDriveContentTemplateSelector.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace LiveConnectExample
+{
+    public class SkyDriveContentTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FolderOrAlbumTemplate { get; set; }
+
+        public DataTemplate AudioVideoTemplate { get; set; }
+
+        public DataTemplate PhotoTemplate { get; set; }
+
+        public DataTemplate EmbeddableFileTemplate { get; set; }
+
+        /// <summary>
+        /// When implemented by a derived class, returns a specific DataTemplate for a given item or container.
+        /// </summary>
+        /// <param name="item">The item to return a template for.</param>
+        /// <param name="container">The parent container for the templated item.</param>
+        /// <returns>
+        /// The template to use for the given item and/or container.
+        /// </returns>
+        protected override DataTemplate SelectTemplateCore(Object item, DependencyObject container)
+        {
+            var skyDriveItem = (dynamic) item;
+            String itemType = item == null ? String.Empty : skyDriveItem.type.ToString();
+            switch (itemType)
+            {
+                case "folder":
+                case "album":
+                    return FolderOrAlbumTemplate;
+                case "photo":
+                    return PhotoTemplate;
+                case "video":
+                case "audio":
+                    return AudioVideoTemplate;
+                case "file":
+                    return EmbeddableFileTemplate;
+                default:
+                    return base.SelectTemplateCore(item, container);
+            }
+        }
+    }
+}
