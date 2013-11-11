d1d532fd7de78cc88c35f3e49ac7e836c610ca74
avidgator_cp <SND\avidgator_cp>
2013-11-11 04:43:03 +0000

    Moved push oconstants into their own file.

---
+++ WinRTByExample81/PushNotificationExample/PushNotificationHelpers/NotificationConstants.cs
@@ -0,0 +1,26 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PushNotificationHelper.cs" company="Jeremy Likness">
+//   Copyright (c) Jeremy Likness
+// </copyright>
+// <summary>
+//   The push notification helper.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace PushNotificationExample
+{
+    using System;
+
+    internal class NotificationConstants
+    {
+        /// <summary>
+        /// Package security identifier
+        /// </summary>
+        internal const String PackageSecurityIdentifier = "YOUR_PACKAGE_SID_HERE";
+
+        /// <summary>
+        /// Client secret
+        /// </summary>
+        internal const String ClientSecret = "YOUR_CLIENT_SECRET_HERE";
+    }
+}
