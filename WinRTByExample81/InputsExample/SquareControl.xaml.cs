709c668787973940e49c13585ab3c1d300d67f61
John Garland <dotnetgator@users.noreply.github.com>
2014-01-02 02:02:35 +0000

    Adding Chapter 13 Inputs Example

---
+++ WinRTByExample81/InputsExample/SquareControl.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace InputsExample
+{
+    public sealed partial class SquareControl : UserControl
+    {
+        private readonly InputEventHandler _inputEventHandler;
+        public SquareControl()
+        {
+            InitializeComponent();
+            _inputEventHandler = new InputEventHandler(InnerShape);
+        }
+    }
+}
