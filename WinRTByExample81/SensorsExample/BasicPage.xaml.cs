cfd28a818f3f012476cb706c5e8010c484eb459f
John Garland <dotnetgator@users.noreply.github.com>
2014-01-06 05:28:37 +0000

    Adding Sensors Example for Chapter 13

---
+++ WinRTByExample81/SensorsExample/BasicPage.xaml.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using SensorsExample.Common;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace SensorsExample
+{
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class BasicPage : Page
+    {
+
+        private readonly NavigationHelper _navigationHelper;
+        private readonly ObservableDictionary _defaultViewModel = new ObservableDictionary();
+        private readonly SensorSettings _sensorSettings;
+        private readonly SensorHelper _sensorHelper;
+        private readonly GeolocationHelper _geolocationHelper;
+
+        private readonly DispatcherTimer _timer = new DispatcherTimer();
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return _defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return _navigationHelper; }
+        }
+
+        public BasicPage()
+        {
+            _sensorSettings = new SensorSettings
+                              {
+                                  AccelerometerReportInterval = 16,
+                                  CompassReportInterval = 16,
+                                  GyrometerReportInterval = 16,
+                                  InclinometerReportInterval = 16,
+                                  LightSensorReportInterval = 16,
+                                  OrientationSensorReportInterval = 16
+                              };
+            _sensorHelper = new SensorHelper(_sensorSettings);
+            _geolocationHelper = new GeolocationHelper(_sensorSettings);
+
+            InitializeComponent();
+            _navigationHelper = new NavigationHelper(this);
+            _navigationHelper.LoadState += HandleNavigationHelperLoadState;
+            _navigationHelper.SaveState += HandleNavigationHelperSaveState;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation. Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session. The state will be null the first time a page is visited.</param>
+        private void HandleNavigationHelperLoadState(Object sender, LoadStateEventArgs e)
+        {
+            DefaultViewModel["SensorSettings"] = _sensorSettings;
+            DefaultViewModel["Position"] = new BasicGeoposition();
+            InitializeTimer();
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void HandleNavigationHelperSaveState(Object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="Common.NavigationHelper.LoadState"/>
+        /// and <see cref="Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            _navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            _navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private async void HandleSimpleOrientationRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var reading = _sensorHelper.GetSimpleOrientationReaading();
+            await ShowMessage(String.Format("SimpleOrientation = {0}", reading));
+            _timer.Start();
+        }
+
+        private async void HandleCompassRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var reading = _sensorHelper.GetCompassReading();
+            await ShowMessage(String.Format("Compass: {0}", reading.DisplayText()));
+            _timer.Start();
+        }
+
+        private async void HandleLightSensorRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var reading = _sensorHelper.GetLightSensorReading();
+            await ShowMessage(String.Format("Light Sensor: {0}", reading.DisplayText()));
+            _timer.Start();
+        }
+
+        private async void HandleAccelerometerRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var reading = _sensorHelper.GetAccelerometerReading();
+            await ShowMessage(String.Format("Accelerometer: {0}", reading.DisplayText()));
+            _timer.Start();
+        }
+
+        private async void HandleGyrometerRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var reading = _sensorHelper.GetGyrometerReading();
+            await ShowMessage(String.Format("Gyrometer: {0}", reading.DisplayText()));
+            _timer.Start();
+        }
+
+        private async void HandleInclinometerRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var reading = _sensorHelper.GetInclinometerReading();
+            await ShowMessage(String.Format("Inclinometer: {0}", reading.DisplayText()));
+            _timer.Start();
+        }
+
+        private async void HandleOrientationSensorRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var reading = _sensorHelper.GetOrientationSensorReading();
+            await ShowMessage(String.Format("Orientation Sensor: {0}", reading.DisplayText()));
+            _timer.Start();
+        }
+
+        private async void HandleShowCurentLocationRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var position = await _geolocationHelper.GetPosition();
+            await ShowMessage(String.Format("Location: {0}",
+                position.Coordinate.Point.Position.DisplayText()));
+            _timer.Start();
+        }
+
+        private async void HandleCenterOnLocationRequest(Object sender, RoutedEventArgs e)
+        {
+            _timer.Stop();
+            var position = await _geolocationHelper.GetPosition();
+            await Dispatcher.Dispatch(async () =>
+            {
+                if (position == null)
+                {
+                    await ShowMessage("No Position returned.");
+                }
+                else
+                {
+                    DefaultViewModel["Position"] = position.Coordinate.Point.Position;
+                }
+            });
+            _timer.Start();
+        }
+
+        private async Task ShowMessage(String message)
+        {
+            var messageDialog = new MessageDialog(message, "Sensors");
+            await messageDialog.ShowAsync();
+        }
+
+        private void InitializeTimer()
+        {
+            _timer.Interval = TimeSpan.FromMilliseconds(30);
+            _timer.Start();
+            _timer.Tick += TimerOnTick;
+        }
+
+        private void TimerOnTick(Object sender, Object o)
+        {
+            _timer.Stop();
+            if (_sensorSettings.FollowInclinometer)
+            {
+                var inclinometerReading = _sensorSettings.LatestInclinometerReading;
+
+                var position = (BasicGeoposition)DefaultViewModel["Position"];
+                var factor = 50/myMap.ZoomLevel;
+                var newlatitude = (position.Latitude + (factor * Math.Sin(inclinometerReading.PitchDegrees * Math.PI / 180))).Between(-90, 90);
+                var newlongitude = (position.Longitude + (factor * Math.Sin(inclinometerReading.RollDegrees * Math.PI / 180))) % 180;
+                var newPosition = new BasicGeoposition
+                {
+                    Altitude = position.Altitude,
+                    Latitude = newlatitude,
+                    Longitude = newlongitude
+                };
+                DefaultViewModel["Position"] = newPosition;
+            }
+
+            if (_sensorSettings.IsFollowingCompass)
+            {
+                var compassReading = _sensorSettings.LatestCompassReading;
+                DefaultViewModel["Heading"] = compassReading.HeadingMagneticNorth;
+            }
+
+            _timer.Start();
+        }
+    }
+
+    public static class Extensions
+    {
+        public static Double Between(this Double value, Double minValue, Double maxValue)
+        {
+            if (minValue > maxValue) throw new InvalidOperationException("Max must be greater than min.");
+            var result = Math.Min(value, maxValue);
+            result = Math.Max(result, minValue);
+            return result;
+        }
+
+        public static async Task Dispatch(this CoreDispatcher dispatcher, Action dispatchAction)
+        {
+            await dispatcher.RunAsync(CoreDispatcherPriority.Normal, dispatchAction.Invoke);
+        }
+    }
+}
---
+++ WinRTByExample81/SensorsExample/SensorSettings.cs
@@ -0,0 +1,370 @@
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using Windows.Devices.Geolocation;
+using Windows.Devices.Sensors;
+using SensorsExample.Annotations;
+
+namespace SensorsExample
+{
+    public class SensorSettings : INotifyPropertyChanged
+    {
+        #region Fields
+
+        private readonly SynchronizationContext _context = SynchronizationContext.Current;
+
+        private Boolean _isSimpleOrientationAvailable;
+        private SimpleOrientation _latestSimpleOrientationReading;
+
+        private Boolean _isCompassAvailable;
+        private Boolean _isFollowingCompass;
+        private CompassReading _latestCompassReading;
+
+        private Boolean _isLightSensorAvailable;
+        private LightSensorReading _latestLightSensorReading;
+
+        private Boolean _isAccelerometerAvailable;
+        private AccelerometerReading _latestAccelerometerReading;
+
+        private Boolean _isGyrometerAvailable;
+        private GyrometerReading _latestGyrometerReading;
+
+        private Boolean _isInclinometerAvailable;
+        private Boolean _followInclinometer;
+        private InclinometerReading _latestInclinometerReading;
+
+        private Boolean _isLocationAvailable;
+        private BasicGeoposition _latestLocationReading;
+
+        private Boolean _isOrientationSensorAvailable;
+        private OrientationSensorReading _latestOrientationSensorReading;
+
+        #endregion
+
+        #region ReportIntervals
+
+        public UInt32 CompassReportInterval { get; set; }
+        public UInt32 LightSensorReportInterval { get; set; }
+        public UInt32 AccelerometerReportInterval { get; set; }
+        public UInt32 GyrometerReportInterval { get; set; }
+        public UInt32 InclinometerReportInterval { get; set; }
+        public UInt32 OrientationSensorReportInterval { get; set; } 
+
+        #endregion
+
+        #region Simple Orientation
+
+        public Boolean IsSimpleOrientationAvailable
+        {
+            get { return _isSimpleOrientationAvailable; }
+            set
+            {
+                if (value.Equals(_isSimpleOrientationAvailable)) return;
+                _isSimpleOrientationAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SimpleOrientation LatestSimpleOrientationReading
+        {
+            get { return _latestSimpleOrientationReading; }
+            set
+            {
+                if (value == _latestSimpleOrientationReading) return;
+                _latestSimpleOrientationReading = value;
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region Compass
+
+        public Boolean IsCompassAvailable
+        {
+            get { return _isCompassAvailable; }
+            set
+            {
+                if (value.Equals(_isCompassAvailable)) return;
+                _isCompassAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Boolean IsFollowingCompass
+        {
+            get { return _isFollowingCompass; }
+            set
+            {
+                if (value.Equals(_isFollowingCompass)) return;
+                _isFollowingCompass = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public CompassReading LatestCompassReading
+        {
+            get { return _latestCompassReading; }
+            set
+            {
+                if (Equals(value, _latestCompassReading)) return;
+                _latestCompassReading = value;
+                OnPropertyChanged();
+                OnPropertyChanged("LatestCompassReadingText");
+            }
+        }
+
+        public String LatestCompassReadingText
+        {
+            get
+            {
+                if (LatestCompassReading == null) return "No Reading Available.";
+                return LatestCompassReading.DisplayText();
+            }
+        }
+
+        #endregion
+
+        #region Light Sensor
+
+        public Boolean IsLightSensorAvailable
+        {
+            get { return _isLightSensorAvailable; }
+            set
+            {
+                if (value.Equals(_isLightSensorAvailable)) return;
+                _isLightSensorAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public LightSensorReading LatestLightSensorReading
+        {
+            get { return _latestLightSensorReading; }
+            set
+            {
+                if (value == _latestLightSensorReading) return;
+                _latestLightSensorReading = value;
+                OnPropertyChanged();
+                OnPropertyChanged("LatestLightSensorReadingText");
+            }
+        }
+
+        public String LatestLightSensorReadingText
+        {
+            get
+            {
+                if (LatestLightSensorReading == null) return "No Reading Available.";
+                return LatestLightSensorReading.DisplayText();
+            }
+        }
+
+        #endregion
+
+        #region Accelerometer
+
+        public Boolean IsAccelerometerAvailable
+        {
+            get { return _isAccelerometerAvailable; }
+            set
+            {
+                if (value.Equals(_isAccelerometerAvailable)) return;
+                _isAccelerometerAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public AccelerometerReading LatestAccelerometerReading
+        {
+            get { return _latestAccelerometerReading; }
+            set
+            {
+                if (value == _latestAccelerometerReading) return;
+                _latestAccelerometerReading = value;
+                OnPropertyChanged();
+                OnPropertyChanged("LatestAccelerometerReadingText");
+            }
+        }
+
+        public String LatestAccelerometerReadingText
+        {
+            get
+            {
+                if (LatestAccelerometerReading == null) return "No Reading Available.";
+                return LatestAccelerometerReading.DisplayText();
+            }
+        }
+
+        #endregion
+
+        #region Gyrometer
+
+        public Boolean IsGyrometerAvailable
+        {
+            get { return _isGyrometerAvailable; }
+            set
+            {
+                if (value.Equals(_isGyrometerAvailable)) return;
+                _isGyrometerAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public GyrometerReading LatestGyrometerReading
+        {
+            get { return _latestGyrometerReading; }
+            set
+            {
+                if (value == _latestGyrometerReading) return;
+                _latestGyrometerReading = value;
+                OnPropertyChanged();
+                OnPropertyChanged("LatestGyrometerReadingText");
+            }
+        }
+
+        public String LatestGyrometerReadingText
+        {
+            get
+            {
+                if (LatestGyrometerReading == null) return "No Reading Available.";
+                return LatestGyrometerReading.DisplayText();
+            }
+        }
+
+        #endregion
+
+        #region Inclinometer
+
+        public Boolean IsInclinometerAvailable
+        {
+            get { return _isInclinometerAvailable; }
+            set
+            {
+                if (value.Equals(_isInclinometerAvailable)) return;
+                _isInclinometerAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Boolean FollowInclinometer
+        {
+            get { return _followInclinometer; }
+            set
+            {
+                if (value.Equals(_followInclinometer)) return;
+                _followInclinometer = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public InclinometerReading LatestInclinometerReading
+        {
+            get { return _latestInclinometerReading; }
+            set
+            {
+                if (Equals(value, _latestInclinometerReading)) return;
+                _latestInclinometerReading = value;
+                OnPropertyChanged();
+                OnPropertyChanged("InclinometerReadingText");
+            }
+        }
+
+        public String InclinometerReadingText
+        {
+            get
+            {
+                if (LatestInclinometerReading == null) return "No Reading Available.";
+                return LatestInclinometerReading.DisplayText();
+            }
+        }
+
+        #endregion
+
+        #region Location
+
+        public Boolean IsLocationAvailable
+        {
+            get { return _isLocationAvailable; }
+            set
+            {
+                if (value.Equals(_isLocationAvailable)) return;
+                _isLocationAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public BasicGeoposition LatestLocationReading
+        {
+            get { return _latestLocationReading; }
+            set
+            {
+                if (value.Equals(_latestLocationReading)) return;
+                _latestLocationReading = value;
+                OnPropertyChanged();
+                OnPropertyChanged("LatestLocationReadingText");
+            }
+        }
+
+        public String LatestLocationReadingText
+        {
+            get { return LatestLocationReading.DisplayText(); }
+        }
+
+        #endregion
+
+        #region Orientation Sensor
+
+        public Boolean IsOrientationSensorAvailable
+        {
+            get { return _isOrientationSensorAvailable; }
+            set
+            {
+                if (value.Equals(_isOrientationSensorAvailable)) return;
+                _isOrientationSensorAvailable = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public OrientationSensorReading LatestOrientationSensorReading
+        {
+            get { return _latestOrientationSensorReading; }
+            set
+            {
+                if (value == _latestOrientationSensorReading) return;
+                _latestOrientationSensorReading = value;
+                OnPropertyChanged();
+                OnPropertyChanged("LatestOrientationSensorReadingText");
+            }
+        }
+
+        public String LatestOrientationSensorReadingText
+        {
+            get
+            {
+                if (LatestOrientationSensorReading == null) return "No Reading Available.";
+                return LatestOrientationSensorReading.DisplayText();
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged Implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] String propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                // Use the sync context to martial the raising of the property change notification, 
+                // since some of these changes are triggered by sensor events that occur off of the UI thread.
+                _context.Post(x => handler(this, new PropertyChangedEventArgs(propertyName)), null);
+            }
+        } 
+
+        #endregion
+    }
+}
