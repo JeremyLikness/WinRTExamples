8f7950262c9a24a9a14eca16a7510162ef6eae00
Jeremy Likness <jeremy@jeremylikness.com>
2014-01-11 15:13:12 +0000

    Add push notification helper

---
+++ WinRTByExample81/PushNotificationHelper/OAuthToken.cs
@@ -0,0 +1,13 @@
+﻿namespace PushNotificationHelper
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class OAuthToken
+    {
+        [DataMember(Name = "access_token")]
+        public string AccessToken { get; set; }
+        [DataMember(Name = "token_type")]
+        public string TokenType { get; set; }
+    }
+}
