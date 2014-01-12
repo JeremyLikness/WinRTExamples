a9592d2964068be2602a678577140930487ce52f
jeremylikness_cp <SND\jeremylikness_cp>
2014-01-12 20:21:14 +0000

    Remove store association

--- WinRTByExample81/LockScreenTasks/RawNotificationTask.cs
+++ WinRTByExample81/LockScreenTasks/RawNotificationTask.cs
@@ -7,15 +7,40 @@
     using Windows.Storage;
     using Windows.UI.Notifications;
 
+    using WinRTByExample.NotificationHelper.Badges;
     using WinRTByExample.NotificationHelper.Toasts;
 
     public sealed class RawNotificationTask : IBackgroundTask
     {
-        private const string MessageKey = "RawMessage"; 
+        private const string MessageKey = "RawMessage";
+
+        private const string CountKey = "MessageCount";
 
         public static string GetMessageKey()
         {
             return MessageKey;
+        }
+
+        public static string GetCountKey()
+        {
+            return CountKey;
+        }
+
+        private int MessageCount
+        {
+            get
+            {
+                if (!ApplicationData.Current.LocalSettings.Values.ContainsKey(CountKey))
+                {
+                    ApplicationData.Current.LocalSettings.Values[CountKey] = 0;
+                }
+                return int.Parse(ApplicationData.Current.LocalSettings.Values[CountKey].ToString());
+            }
+
+            set
+            {
+                ApplicationData.Current.LocalSettings.Values[CountKey] = value;
+            }
         }
 
         public void Run(IBackgroundTaskInstance taskInstance)
@@ -28,6 +53,9 @@
             var content = string.Format("{0}: {1}", DateTime.Now, notification.Content);
             ApplicationData.Current.LocalSettings.Values[MessageKey] = content;
             ToastTemplateType.ToastText01.GetToast().AddText(content).Send();
+            var count = MessageCount + 1;
+            (count > 99 ? 99 : count).GetBadge().Set();
+            MessageCount = count;
         }
     }
 }
