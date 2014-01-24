c55d242e7fbda73d25e1d531776f97b1d12514a2
Jeremy Likness <jeremy@jeremylikness.com>
2014-01-24 00:07:59 +0000

    Accessibility example for Chapter 17

---
+++ WinRTByExample81/AccessibilityExample/ViewModel.cs
@@ -0,0 +1,38 @@
+﻿namespace AccessibilityExample
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    using Windows.UI.Xaml;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private ElementTheme currentTheme = Application.Current.RequestedTheme == ApplicationTheme.Dark ? ElementTheme.Dark : ElementTheme.Light;
+
+        public ElementTheme CurrentTheme
+        {
+            get
+            {
+                return currentTheme;
+            }
+
+            set
+            {
+                currentTheme = value; 
+                this.OnPropertyChanged();
+            }
+        }
+
+        public void ToggleTheme()
+        {
+            this.CurrentTheme = this.currentTheme == ElementTheme.Dark ? ElementTheme.Light : ElementTheme.Dark;            
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));           
+        }
+    }
+}
