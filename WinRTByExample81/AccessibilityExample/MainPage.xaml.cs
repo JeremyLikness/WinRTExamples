a24a695cac4876be73b5bca34d4867777f0c0c56
jeremylikness_cp <SND\jeremylikness_cp>
2014-01-24 00:07:59 +0000

    Accessibility example for Chapter 17

---
+++ WinRTByExample81/AccessibilityExample/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace AccessibilityExample
+{
+    using Windows.UI.Xaml;
+
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage
+    {
+        private ViewModel viewModel;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Loaded += (o, e) =>
+                {
+                    this.DataContext = viewModel = viewModel ?? new ViewModel();                    
+                };
+        }
+
+        private void ChangeThemeOnClick(object sender, RoutedEventArgs e)
+        {
+            viewModel.ToggleTheme();
+        }
+    }
+}
