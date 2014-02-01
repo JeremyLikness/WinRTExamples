8f112f4f8dfb88c7752b2bb3432f94121c4a2b77
jeremylikness_cp <SND\jeremylikness_cp>
2014-02-01 15:49:04 +0000

    More accessibility changes

--- WinRTByExample81/AccessibilityExample/ViewModel.cs
+++ WinRTByExample81/AccessibilityExample/ViewModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace AccessibilityExample
 {
+    using System.Collections;
+    using System.Collections.Generic;
     using System.ComponentModel;
+    using System.Linq;
     using System.Runtime.CompilerServices;
 
     using Windows.Storage;
@@ -9,6 +12,20 @@
     public class ViewModel : INotifyPropertyChanged
     {
         private ElementTheme currentTheme = Application.Current.RequestedTheme == ApplicationTheme.Dark ? ElementTheme.Dark : ElementTheme.Light;
+
+        private readonly IEnumerable<Item> items = new List<Item>
+                                              {
+                                                  new Item { Id = 1, Description = "List Item 1" },
+                                                  new Item { Id = 2, Description = "List Item 2" },
+                                                  new Item { Id = 3, Description = "List Item 3" }
+                                              };
+
+        private Item currentItem, currentItem2;
+
+        public ViewModel()
+        {
+            this.currentItem = this.currentItem2 = this.items.First();
+        }
 
         public ElementTheme CurrentTheme
         {
@@ -21,6 +38,44 @@
             {
                 currentTheme = value; 
                 this.OnPropertyChanged();
+            }
+        }
+
+        public Item CurrentItem
+        {
+            get
+            {
+                return currentItem;
+            }
+
+            set
+            {
+                currentItem = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        public Item CurrentItem2
+        {
+            get
+            {
+                return currentItem2;
+            }
+
+            set
+            {
+                currentItem2 = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+
+
+        public IEnumerable<Item> Items
+        {
+            get
+            {
+                return this.items;
             }
         }
 
