fd4baa19628ab56e2e289ccf9814517f755eb94b
jeremylikness_cp <SND\jeremylikness_cp>
2014-02-13 01:13:14 +0000

    Add viewmodel example

--- WinRTByExample81/GlobalizationExample/MainPage.xaml.cs
+++ WinRTByExample81/GlobalizationExample/MainPage.xaml.cs
@@ -1,29 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
-
 namespace GlobalizationExample
 {
+    using Windows.UI.Xaml.Navigation;
     using Windows.ApplicationModel.Resources;
     using Windows.Globalization.NumberFormatting;
 
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class MainPage
     {
         const double CurrencyValue = 123.45;
             
--- WinRTByExample81/GlobalizationExample/ViewModel.cs
+++ WinRTByExample81/GlobalizationExample/ViewModel.cs
@@ -1,12 +1,58 @@
 ﻿namespace GlobalizationExample
 {
+    using System;
+    using System.Collections.Generic;
+
+    using Windows.ApplicationModel;
+    using Windows.ApplicationModel.Resources;
+    using Windows.Globalization.NumberFormatting;
+
     public class ViewModel
     {
+        private const double Number = Math.PI;
+
+        private readonly ResourceLoader resourceLoader;
+
+        private readonly string geographicRegion;
+
+        private readonly IReadOnlyList<string> languages;
+        
+
+        public ViewModel()
+        {
+            if (DesignMode.DesignModeEnabled)
+            {
+                this.resourceLoader = new ResourceLoader("Resources");
+                this.geographicRegion = "US";
+                this.languages = new[] { "en-US" };
+            }
+            else
+            {
+                this.resourceLoader = ResourceLoader.GetForCurrentView();
+                this.geographicRegion = Windows.System.UserProfile.GlobalizationPreferences.HomeGeographicRegion;
+                this.languages = Windows.System.UserProfile.GlobalizationPreferences.Languages;
+            }
+        }
+
         public string SimpleDate
         {
             get
             {
                 return "2014-02-08";
+            }
+        }
+
+        public string FormattedNumber
+        {
+            get
+            {
+                var decimalFormatter =
+                    new DecimalFormatter(this.languages, this.geographicRegion);
+                var percentageFormatter = new PercentFormatter(this.languages, this.geographicRegion);
+                return string.Format(
+                    this.resourceLoader.GetString("FormattedNumber"),
+                    decimalFormatter.FormatDouble(Number),
+                    percentageFormatter.FormatDouble(Number));
             }
         }
     }
